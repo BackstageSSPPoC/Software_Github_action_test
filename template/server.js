60dc274104b348ace510b8224fef7ca309b1284f
Chaitanya Shekhar Pandey <raichaitanya2920@gmail.com>
2026-02-24 06:26:44 +0000

    Yaml change

--- template/server.js
+++ template/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Welcome to Backstage Self Service Portal!\n");
+  res.end("Welcome to Backstage Self Service Portal -- Github Action!\n");
 });
 
 const PORT = 3000;
